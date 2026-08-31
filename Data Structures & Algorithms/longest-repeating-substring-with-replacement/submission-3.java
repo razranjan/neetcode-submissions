ea9ca6741e2b4ecb865c754039f94092cd2c2b7a
razranjan <150904676+razranjan@users.noreply.github.com>
2026-09-01 01:47:33 +0530

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int maxLen = 0;
+
+        int left = 0;
+        int right = 0;
+        int hash[] = new int[26];
+
+        int maxFreq = 0;
+        while(left <= right && right < s.length()){
+
+            hash[s.charAt(right) - 'A']++;
+            maxFreq = findMaxFreq(hash);
+            //System.out.println("maxFreq coming for pos flow" + maxFreq);
+
+             if ((right - left+1) - maxFreq <= k){
+                maxLen = Math.max(maxLen, right - left+1);
+                //System.out.println("Maxlen coming from >> " + right+" :" + left);
+             }
+
+             else {
+                while((right - left+1) - maxFreq > k){
+                     //System.out.println("Inside neg case >> " + right+" :" + left);
+                    hash[s.charAt(left) - 'A']--;
+                    maxFreq = findMaxFreq(hash);
+                    //System.out.println("maxFreq coming for neg flow" + maxFreq);
+                    left++;
+                }
+             }
+
+            right++;
+
+        }
+
+        return maxLen;
+
+    }
+
+
+    int findMaxFreq(int[] hash){
+
+        int maxFreq = 0;
+
+        for (int index = 0; index < 26; index++){
+            maxFreq = Math.max(maxFreq, hash[index]);
+        }
+
+        return maxFreq;
+    }
+}
